3d71956957dbb32f29046b78c7c8f03fd2b0f018
ddang8-jpeg <parkrangerdang@gmail.com>
2025-01-25 16:34:58 -0500

    Added parallax scrolling to camera results.

--- app/(tabs)/camera.js
+++ app/(tabs)/camera.js
@@ -7,6 +7,8 @@ import {
   StyleSheet,
   ActivityIndicator,
 } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
 import * as ImagePicker from "expo-image-picker";
 import Tesseract from "tesseract.js";
 
@@ -14,6 +16,7 @@ export default function CameraScreen() {
   const [imageUri, setImageUri] = useState(null);
   const [text, setText] = useState("");
   const [isProcessing, setIsProcessing] = useState(false);
+  const [finishedProcessing, setfinishedProcessing] = useState(false);
 
   const pickImage = async () => {
     // Request permission to access the gallery
@@ -40,6 +43,7 @@ export default function CameraScreen() {
 
   const processImage = async (uri) => {
     setIsProcessing(true);
+
     try {
       const { data } = await Tesseract.recognize(uri, "eng", {
         logger: (info) => console.log(info), // Log progress
@@ -50,19 +54,32 @@ export default function CameraScreen() {
       setText("Could not process the image.");
     } finally {
       setIsProcessing(false);
+      setfinishedProcessing(true);
     }
   };
 
   return (
     <View style={styles.container}>
-      <Button title="Pick an Image" onPress={pickImage} />
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
       {isProcessing ? (
         <ActivityIndicator size="large" color="#0000ff" />
+      ) : finishedProcessing ? (
+        <ParallaxScrollView
+          headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+          headerImage={
+            <Image source={{ uri: imageUri }} style={styles.image} />
+          }
+        >
+          <ThemedText>{text}</ThemedText>
+        </ParallaxScrollView>
       ) : (
-        <Text style={styles.text}>
-          {text || "Recognized text will appear here"}
-        </Text>
+        <View>
+          <Button title="Pick an Image" onPress={pickImage} />
+          {imageUri && (
+            <Image source={{ uri: imageUri }} style={styles.image} />
+          )}
+
+          <ThemedText>{"Recognized text will appear here"}</ThemedText>
+        </View>
       )}
     </View>
   );
@@ -80,6 +97,7 @@ const styles = StyleSheet.create({
     height: 300,
     marginVertical: 20,
     borderRadius: 10,
+    alignItems: "center",
   },
   text: {
     marginTop: 20,
